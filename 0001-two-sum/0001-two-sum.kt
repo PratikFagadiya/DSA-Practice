0293f180257a546e3789c360128ae55edf0b9e7f
Pratik Fagadiya <66842132+PratikFagadiya@users.noreply.github.com>
2026-08-31 21:07:00 +0530

    Time: 3 ms (62.26%), Space: 46.8 MB (83.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,15 +1,15 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val map = mutableMapOf<Int,Int>()
+        val map = mutableMapOf<Int, Int>()
 
-        for(i in 0 until nums.size) {
-            val remaining = target - nums[i]
-            if(map.containsKey(remaining)) {
-                return intArrayOf(map[remaining]!!, i)
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+
+            if (map.containsKey(complement)) {
+                return intArrayOf(map[complement]!!, i)
             }
 
             map[nums[i]] = i
-
         }
 
         return intArrayOf()
